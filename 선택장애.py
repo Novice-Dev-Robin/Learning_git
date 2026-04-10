ca5b84e0b460643d729caa3729a3c2fd992e913b
Novice-Dev-Robin <dev.robin03@gmail.com>
2026-04-10 21:26:39 +0900

    진짜로 완성

--- 선택장애.py
+++ 선택장애.py
@@ -5,7 +5,7 @@ def generate_random_numbers(n, lower_bound, upper_bound):
         num = random.randint(lower_bound, upper_bound)
     return num
 
-if generate_random_numbers(1,1,2) % 2 == 0:
+if generate_random_numbers(1,1,50) % 2 == 0:
     print("옷 입어라 나가게.")
 else:
     print("얌전히 집에 박혀 있어.")
