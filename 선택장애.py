d8cfd244580efceb48d45d1e3c7f64e67fc348ea
Novice-Dev-Robin <dev.robin03@gmail.com>
2026-04-10 21:23:23 +0900

    수정

--- 선택장애.py
+++ 선택장애.py
@@ -7,7 +7,7 @@ def generate_random_numbers(n, lower_bound, upper_bound):
         random_numbers.append(num)
     return random_numbers
 
-if generate_random_numbers(1,1,4) == [1]:
+if generate_random_numbers(1,1,2) == [1]:
     print("옷 입어라 나가게.")
 else:
     print("얌전히 집에 박혀 있어.")
