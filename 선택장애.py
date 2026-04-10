7b15971160a6d4314e1bab70293ea7c09680cfc2
Novice-Dev-Robin <dev.robin03@gmail.com>
2026-04-10 21:25:57 +0900

    완성.

--- 선택장애.py
+++ 선택장애.py
@@ -1,13 +1,11 @@
 import random
 
 def generate_random_numbers(n, lower_bound, upper_bound):
-    random_numbers = []
     for _ in range(n):
         num = random.randint(lower_bound, upper_bound)
-        random_numbers.append(num)
-    return random_numbers
+    return num
 
-if generate_random_numbers(1,1,2) == [1]:
+if generate_random_numbers(1,1,2) % 2 == 0:
     print("옷 입어라 나가게.")
 else:
     print("얌전히 집에 박혀 있어.")
