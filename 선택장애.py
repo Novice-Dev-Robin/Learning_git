fff731a2e2d568c1a5bffe48d8e0cdf2d9f30776
Novice-Dev-Robin <dev.robin03@gmail.com>
2026-04-10 21:21:53 +0900

    두 개중 하나, 50퍼 확률

--- 선택장애.py
+++ 선택장애.py
@@ -7,4 +7,8 @@ def generate_random_numbers(n, lower_bound, upper_bound):
         random_numbers.append(num)
     return random_numbers
 
-print(generate_random_numbers(3,1,10))
+print(generate_random_numbers(1,1,2))
+if generate_random_numbers(1,1,4) == [1]:
+    print("옷 입어라 나가게.")
+else:
+    print("얌전히 집에 박혀 있어.")
